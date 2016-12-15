98ddc2c4e4ca0cef6c1aac4997dc3169b192079f
DESKTOP-N6D8V1D\Sambuddha Ghosal <sghosal@iastate.edu>
2016-12-15 09:11:04 -0600

    ME_557 Project Complete

---
+++ ME557_Project_Sambuddha_Ghosal/SolarSystem/globals.h
@@ -0,0 +1,14 @@
+// Sambuddha Ghosal ME 557 Project
+
+#ifndef SAM_GLOBALS_H
+#define SAM_GLOBALS_H
+
+// This scales down the distances of planets in the simulation. For example, if the scale is 0.00000001
+// 149.6 million km from the sun = 1.496 units in 3d from the sun
+const float distanceScale = 0.00000001f;
+
+// Planet radius of 6,371km and scale of 0.0001f = radius of 0.6371 units
+
+extern float planetSizeScale;
+
+#endif
